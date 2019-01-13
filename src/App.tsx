fbf69c535e34fcb0b959d032ba6b89950dbb1de1
Sean <toranos@gmail.com>
2019-01-13 22:09:53 +1100

    Add tslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,101 @@
-import React, { Component, ChangeEventHandler, MouseEventHandler } from 'react';
-import {DragDropContext, Droppable, Draggable, OnDragEndResponder} from 'react-beautiful-dnd';
 import 'milligram';
 import 'normalize.css';
+import React, { ChangeEventHandler, Component, MouseEventHandler } from 'react';
+import { DragDropContext, Draggable, Droppable, OnDragEndResponder } from 'react-beautiful-dnd';
 import './App.css';
 import { Canvas } from './Canvas';
+
+const reorder = <T extends {}>(arr: T[], srcIndex: number, destIndex: number): T[] => {
+  const arrCopy = [...arr];
+  const [moved] = arrCopy.splice(srcIndex, 1);
+  arrCopy.splice(destIndex, 0, moved);
+
+  return arrCopy;
+};
 
 interface Props {}
 
 interface State {
-  imgs: {
-    name: string;
+  canvasUrl: string | null;
+  imgs: Array<{
     img: HTMLImageElement;
-  }[],
-  remoteUrl: string|null;
+    name: string;
+  }>;
+  remoteUrl: string | null;
   spacingInput: string;
-  canvasUrl: string|null;
 }
 
-class App extends Component<Props, State> {
+export class App extends Component<Props, State> {
   public state: State = {
+    canvasUrl: null,
     imgs: [],
     remoteUrl: '',
     spacingInput: '40',
-    canvasUrl: null,
-  }
+  };
 
   private fileInput = React.createRef<HTMLInputElement>();
 
-  addLocal = () => {
+  public render() {
+    const { remoteUrl, spacingInput, canvasUrl, imgs } = this.state;
+    const spacing = parseInt(this.state.spacingInput, 10) || 0;
+
+    return (
+      <div className='main container'>
+        <h1>Article Header Generator</h1>
+        <Canvas
+          className='main-canvas'
+          imgs={imgs.map(({img}) => img)}
+          spacing={spacing}
+          onUrlChange={this.onUrlChange}
+        />
+        {this.state.imgs.map(img => <p key={img.img.src}>{img.name}</p>) /* TODO: proper component */}
+        <DragDropContext onDragEnd={this.onDragEnd}>
+          <Droppable droppableId='droppable'>
+            {(provided, snapshot) => (
+              <div
+                ref={provided.innerRef}
+              >
+                {this.state.imgs.map((item, index) => (
+                  <Draggable key={item.img.src} draggableId={item.img.src} index={index}>
+                    {(providedDraggable, snapshotDraggable) => (
+                      <div
+                        ref={providedDraggable.innerRef}
+                        {...providedDraggable.draggableProps}
+                        {...providedDraggable.dragHandleProps}
+                      >
+                        {item.name}
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+
+        <input
+          id='fileInput'
+          type='file'
+          accept='image/*'
+          multiple ref={this.fileInput}
+          hidden
+          onChange={this.addLocal}
+        />
+        <label htmlFor='fileInput' className='button'>Add local images</label>
+        <button className='button button-outline' onClick={this.addRemote}>Add from URL</button>
+        <button className='button button-clear' onClick={this.clear}>Clear</button>
+        {remoteUrl !== null && <input type='text' onChange={this.changeUrl} value={remoteUrl} />}
+        <input type='number' onChange={this.changeSpacing} value={spacingInput}  />
+        {canvasUrl ?
+          <a className='button' href={canvasUrl} download='header.png'>Save</a> :
+          <a className='button disabled-link-btn'>Save</a>
+        }
+      </div>
+    );
+  }
+
+  private addLocal = () => {
     const fileInput = this.fileInput.current;
     if (!fileInput) {
       return;
@@ -47,8 +115,8 @@ class App extends Component<Props, State> {
       img.addEventListener('load', () => {
         loaded += 1;
         if (loaded === toLoad) {
-          // all loaded
-          this.setState({ 
+          // All loaded
+          this.setState({
             imgs: [...this.state.imgs, ...imgs],
           });
         }
@@ -58,13 +126,13 @@ class App extends Component<Props, State> {
         img,
         name: f.name,
       };
-    })
+    });
 
-    // clear input
+    // Clear input
     fileInput.value = '';
   }
 
-  addRemote = () => {
+  private addRemote = () => {
     const { imgs, remoteUrl } = this.state;
     if (remoteUrl === null) {
       throw new Error();
@@ -82,39 +150,32 @@ class App extends Component<Props, State> {
           name,
         }],
         remoteUrl: '',
-      })
+      });
     });
   }
 
-  clear: MouseEventHandler<HTMLButtonElement> = () => {
+  private changeSpacing: ChangeEventHandler<HTMLInputElement> = e => {
+    const spacingInput = e.target.value;
+    this.setState({ spacingInput });
+  }
+
+  private changeUrl: ChangeEventHandler<HTMLInputElement> = e => {
+    this.setState({ remoteUrl: e.target.value });
+  }
+
+  private clear: MouseEventHandler<HTMLButtonElement> = () => {
     this.setState({
       imgs: [],
     });
   }
 
-  changeUrl: ChangeEventHandler<HTMLInputElement> = e => {
-    this.setState({ remoteUrl: e.target.value });
-  }
-
-  changeSpacing: ChangeEventHandler<HTMLInputElement> = e => {
-    const spacingInput = e.target.value;
-    this.setState({ spacingInput });
-  }
-
-  reorder<T>(arr: T[], srcIndex: number, destIndex: number): T[] {
-    const arrCopy = [...arr];
-    const [moved] = arrCopy.splice(srcIndex, 1);
-    arrCopy.splice(destIndex, 0, moved);
-    return arrCopy;
-  }
-
-  onDragEnd: OnDragEndResponder = ({destination, source}) => {
+  private onDragEnd: OnDragEndResponder = ({destination, source}) => {
     if (!destination) {
-      // dropped outside list
+      // Dropped outside list
       return;
     }
 
-    const imgs = this.reorder(
+    const imgs = reorder(
       this.state.imgs,
       destination.index,
       source.index,
@@ -125,61 +186,7 @@ class App extends Component<Props, State> {
     });
   }
 
-  onUrlChange = (canvasUrl: string|null) => {
+  private onUrlChange = (canvasUrl: string | null) => {
     this.setState({ canvasUrl });
   }
-
-  render() {
-    const { remoteUrl, spacingInput, canvasUrl, imgs } = this.state;
-    const spacing = parseInt(this.state.spacingInput) || 0;
-    return (
-      <div className="main container">
-        <h1>Article Header Generator</h1>
-        <Canvas
-          className="main-canvas"
-          imgs={imgs.map(({img}) => img)}
-          spacing={spacing}
-          onUrlChange={this.onUrlChange}
-        />
-        {this.state.imgs.map(img => <p key={img.img.src}>{img.name}</p>) /* TODO: proper component */}
-        <DragDropContext onDragEnd={this.onDragEnd}>
-          <Droppable droppableId="droppable">
-            {(provided, snapshot) => (
-              <div
-                ref={provided.innerRef}
-              >
-                {this.state.imgs.map((item, index) => (
-                  <Draggable key={item.img.src} draggableId={item.img.src} index={index}>
-                    {(provided, snapshot) => (
-                      <div
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                      >
-                        {item.name}
-                      </div>
-                    )}
-                  </Draggable>
-                ))}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-        </DragDropContext>
-
-        <input id="fileInput" type="file" accept="image/*" multiple ref={this.fileInput} hidden onChange={this.addLocal}/>
-        <label htmlFor="fileInput" className="button">Add local images</label>
-        <button className="button button-outline" onClick={this.addRemote}>Add from URL</button>
-        <button className="button button-clear" onClick={this.clear}>Clear</button>
-        {remoteUrl !== null && <input type="text" onChange={this.changeUrl} value={remoteUrl} />}
-        <input type="number" onChange={this.changeSpacing} value={spacingInput}  />
-        {canvasUrl ?
-          <a className="button" href={canvasUrl} download="header.png">Save</a> :
-          <a className="button disabled-link-btn">Save</a>
-        }
-      </div>
-    );
-  }
 }
-
-export default App;
--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,120 +1,123 @@
 import React, { Component } from 'react';
 
 interface Props {
+  className: string;
   imgs: HTMLImageElement[];
   spacing: number;
-  onUrlChange: (canvasUrl: string|null) => void;
-  className: string;
+  onUrlChange(canvasUrl: string | null): void;
 }
 
 interface State {
-  canvasUrl: string|null;
+  canvasUrl: string | null;
 }
+
+const imgsDiffer = (a: HTMLImageElement[], b: HTMLImageElement[]) => {
+  if (a.length !== b.length) {
+    return true;
+  }
+
+  return !a.every((aImg, i) => aImg === b[i]);
+};
+
+const calcPositions = (imgs: HTMLImageElement[], spacing: number) => {
+  const maxHeight = Math.max(...imgs.map(({height}) => height));
+
+  let x = spacing;
+
+  // Calc positions (for drawing later since resizing the canvas clears it)
+  const positions = [];
+  for (const img of imgs) {
+    // TODO: put plus signs in between imgs
+    const verticalWhitespace = 2; // Top and bottom
+    const y = (maxHeight - img.height) / verticalWhitespace;
+    positions.push({ img, x, y });
+    x += img.width + spacing;
+  }
+
+  return {
+    fullWidth: x,
+    positions,
+  };
+};
 
 export class Canvas extends Component<Props> {
   public state: State = {
     canvasUrl: null,
-  }
-  
+  };
+
   private canvas = React.createRef<HTMLCanvasElement>();
-  
-  private imgsDiffer(a: HTMLImageElement[], b: HTMLImageElement[]) {
-    if (a.length !== b.length) {
-      return true;
-    }
-    return !a.every((aImg, i) => aImg === b[i]);
-  }
-  
-  componentDidUpdate(prevProps: Props, prevState: State) {
-    
-    if (this.imgsDiffer(prevProps.imgs, this.props.imgs)) {
+
+  public componentDidUpdate(prevProps: Props, prevState: State) {
+    if (imgsDiffer(prevProps.imgs, this.props.imgs)) {
       this.updateCanvas();
     }
-    
+
     if (prevState.canvasUrl !== this.state.canvasUrl) {
-      // free old url
+      // Free old url
       if (prevState.canvasUrl) {
         URL.revokeObjectURL(prevState.canvasUrl);
       }
       this.props.onUrlChange(this.state.canvasUrl);
     }
   }
-  
-  private calcFullPositions(imgs: HTMLImageElement[], spacing: number) {
-    const maxHeight = Math.max(...imgs.map(({height}) => height));
-    
-    let x = spacing;
-    
-    // calc positions (for drawing later since resizing the canvas clears it)
-    const positions = [];
-    for (const img of imgs) {
-      // TODO: put plus signs in between imgs
-      const y = (maxHeight - img.height) / 2;
-      positions.push({ img, x, y, });
-      x += img.width + spacing;
-    }
-    return {
-      positions,
-      fullWidth: x,
-    }
+
+  public render() {
+    return this.props.imgs.length > 0 && (
+      <canvas ref={this.canvas} className={this.props.className}/>
+    );
   }
-  
+
   private updateCanvas(): void {
-    // clear old url
+    // Clear old url
     this.setState({ canvasUrl: null });
-    
-    const {positions, fullWidth} = this.calcFullPositions(
+
+    const {positions, fullWidth} = calcPositions(
       this.props.imgs,
       this.props.spacing,
     );
     if (positions.length === 0) {
       return;
     }
-    
+
     const mainCanvas = this.canvas.current;
     if (!mainCanvas) {
       throw new Error('mainCanvas missing');
     }
-    
-    // change canvas dims
+
+    // Change canvas dims
     const maxHeight = Math.max(...positions.map(({img}) => img.height));
     mainCanvas.height = maxHeight;
     mainCanvas.width = fullWidth;
-    
+
     const ctx = mainCanvas.getContext('2d');
     if (!ctx) {
       throw new Error('ctx missing');
     }
-    
+
     positions.forEach(({img, x, y}) => ctx.drawImage(
       img,
       x,
       y,
       img.width,
-      img.height
+      img.height,
     ));
-    
+
     this.updateCanvasUrl(mainCanvas);
   }
-      
-  updateCanvasUrl(mainCanvas: HTMLCanvasElement) {
+
+  private updateCanvasUrl(mainCanvas: HTMLCanvasElement) {
     if (!mainCanvas.toBlob) {
-      // toBlob not supported, fall back to data url
+      // Method toBlob not supported, fall back to data url
       this.setState({ canvasUrl: mainCanvas.toDataURL() });
+
       return;
     }
-    
+
     mainCanvas.toBlob(blob => {
       if (!blob) {
         throw new Error();
       }
       this.setState({ canvasUrl: URL.createObjectURL(blob) });
     });
-  }
-  
-  render() {
-    return this.props.imgs.length > 0 && (
-      <canvas ref={this.canvas} className={this.props.className}/>
-    );
   }
 }
