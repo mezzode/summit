fb7af89a7a3dd89941ead6619d92665ab40d9c06
Sean <toranos@gmail.com>
2019-01-21 23:52:23 +1100

    Add GitHub link

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GithubCorner from 'react-github-corner';
 import { App } from './App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <>
+    <GithubCorner
+      href='https://github.com/mezzode/summit'
+      bannerColor='#9b4dca'
+    />
+    <App />
+  </>,
+  document.getElementById('root'),
+);
 
 /* If you want your app to work offline and load faster, you can change
 unregister() to register() below. Note this comes with some pitfalls.
